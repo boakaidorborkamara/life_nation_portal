f35c1b933d6523c2ff78a81d3d9281a21cc1593c
Boakai <boakaidkamara@gmail.com>
2022-07-05 00:35:06 +0000

    created router file for people model

---
+++ route/people_router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const peopleController = require('../controller/peopleController');
+
+
+const router = express.Router();
+
+
+
+//get all churches
+router.get('/people', peopleController.people_list);
+
+//create a new book POST
+router.post('/people/create', peopleController.people_create_post);
+
+//display details for a specific route
+router.get('/people/:id', peopleController.people_detail)
+
+
+
+
+module.exports = router;
