7cb8729524e75e08e512600abf6f92ae7a42ccab
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2024-09-26 02:10:31 +0000

    extract login credential details from form

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,5 +3,23 @@ const login_form = document.getElementById("login-form");
 login_form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  alert("working");
+  let login_info = extractFormData(login_form);
+
+  console.log("login", login_info);
 });
+
+// METHODS -----------------------------------------
+function extractFormData(form) {
+  //   create form data instance
+  let form_data = new FormData(form);
+
+  //   save extracted details
+  let prayer_group_info = {};
+
+  //   extract details from form data
+  for (pair of form_data.entries()) {
+    prayer_group_info[pair[0]] = pair[1];
+  }
+
+  return prayer_group_info;
+}
